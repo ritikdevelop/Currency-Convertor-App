38855d3764ba4efb24899b5632fb6664127e8531
ritikdevelop <hrithikrana216@gmail.com>
2024-07-06 16:11:06 +0530

    Last commit in currency_convertor_app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import ConvertorForm from "./Components/ConvertorForm";
+
+const App = () => {
+  return (
+    <div className="currency-convertor">
+      <h2 className="convertor-title">Currency Convertor</h2>
+      <ConvertorForm />
+    </div>
+  );
+};
+
+export default App;
